e86ab63e90dea53af6567691e8c17e4b3ce0cc49
Tom Nguyen <nhoctom0811@gmail.com>
2017-04-23 23:57:21 +0700

    Create Product Model, Migration and Seeder

---
+++ database/migrations/2017_04_23_165001_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->integer('price');
+            $table->integer('quatity');
+            $table->text('description')->nullable();
+            $table->integer('cat_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('cat_id')
+                  ->reference('id')
+                  ->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}

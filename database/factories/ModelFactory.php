c9def846e78e1d6bf2be3a64ebc2388d164e9f6b
Tom Nguyen <nhoctom0811@gmail.com>
2017-05-16 14:00:45 +0700

    Create Category Model Factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -13,12 +13,21 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(App\User::class, function (Faker\Generator $faker) {
-    static $password;
+  static $password;
 
-    return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
-    ];
+  return [
+      'name' => $faker->name,
+      'email' => $faker->unique()->safeEmail,
+      'password' => $password ?: $password = bcrypt('secret'),
+  ];
+});
+
+/**
+ *  Factory Category
+ */
+$factory->define(App\Category::class, function (Faker\Generator $faker) {
+  return [
+    'name' => $faker->unique()->colorName,
+    'description' => $faker->paragraph,
+  ];
 });
